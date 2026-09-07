2cf29a73c2240f0d656e6528ef599a1203e9d124
napang99 <jiraporn2670@gmail.com>
2026-09-07 11:34:52 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbw7gUWih1UGW37lxpi1jj98G0k35iVN7FVRMP-E4HeMs0ea54oTtAI9f2uj4OYHPx4w/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbwf7LNNg_9sXzKUaUxgjy30it1kZpsq9cnydQi36AeJm6zYXdjyB0VLQAtiUVGgdHAj/exec'
   });
 })();
